99e274c228f552b9c4a7cc082fdefa5f8fe6b6e9
wuYin <wuYinPost@gmail.com>
2018-10-16 20:33:27 +0800

    两数相加

---
+++ linked_list/2_add_two_numbers.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+func main() {
+	l1 := newList([]int{8, 9, 9}) // 5	// 9 8
+	l2 := newList([]int{2})       // 5	// 1
+	cur := addTwoNumbers(l1, l2)
+	for cur != nil {
+		fmt.Print(cur.Val, " ")
+		cur = cur.Next
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 遍历两个链表，取出各自的数字再相加（难以解决整数溢出的问题，1560 / 1563 个通过测试用例，取值求解不可行）
+// 遍历链表，相互相加记进位。注意特殊情况的处理
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var nums []int
+	cur1, cur2 := l1, l2
+	var carryBit bool // 是否要进位
+	for cur1 != nil && cur2 != nil {
+		sum := cur1.Val + cur2.Val
+		if carryBit {
+			sum++
+		}
+		carryBit = false
+		if sum >= 10 {
+			carryBit = true
+		}
+		nums = append(nums, sum%10)
+		cur1 = cur1.Next
+		cur2 = cur2.Next
+	}
+	nums = append(nums, traverse(cur1, carryBit)...)
+	nums = append(nums, traverse(cur2, carryBit)...)
+	if cur1 == nil && cur2 == nil && carryBit {
+		nums = append(nums, 1)
+	}
+	return newList(nums)
+}
+
+func traverse(cur *ListNode, carryBit bool) (remainNums []int) {
+	if cur == nil {
+		return
+	}
+	for cur != nil {
+		if carryBit {
+			res := cur.Val + 1
+			if res >= 10 {
+				remainNums = append(remainNums, 0)
+				carryBit = true
+			} else {
+				remainNums = append(remainNums, res)
+				carryBit = false
+			}
+			cur = cur.Next
+			continue
+		}
+		remainNums = append(remainNums, cur.Val)
+		cur = cur.Next
+	}
+	if carryBit {
+		remainNums = append(remainNums, 1)
+	}
+	return
+}
+
+func newList(nums []int) *ListNode {
+	n := len(nums)
+	if n == 0 {
+		return nil
+	}
+	head := &ListNode{Val: nums[0], Next: nil}
+	cur := head
+	for i := 1; i < n; i++ {
+		newNode := &ListNode{Val: nums[i], Next: nil}
+		cur.Next = newNode
+		cur = newNode
+	}
+	return head
+}
