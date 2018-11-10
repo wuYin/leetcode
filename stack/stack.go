f1a5e531cafe68df7d3fddb19234c8b1e304f449
wuYin <wuYinPost@gmail.com>
2018-11-10 11:04:46 +0800

    实现简单的栈

---
+++ stack/stack.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"log"
+)
+
+// 简单的栈定义
+// go run stack.go n_xxx.go
+type Stack []int
+
+func (s *Stack) Len() int {
+	return len(*s)
+}
+
+func (s *Stack) IsEmpty() bool {
+	if s.Len() == 0 {
+		return true
+	}
+	return false
+}
+
+func (s *Stack) Push(elem int) {
+	*s = append(*s, elem)
+}
+
+var ERR_STACK_EMPTY = errors.New("stack is empty")
+
+func (s *Stack) Pop() int {
+	n := s.Len()
+	if n == 0 {
+		log.Fatal(ERR_STACK_EMPTY)
+	}
+	peek := (*s)[n-1]
+	*s = (*s)[:n-1]
+	return peek
+}
+
+func (s *Stack) Peek() int {
+	n := s.Len()
+	if n == 0 {
+		log.Fatal(ERR_STACK_EMPTY)
+	}
+	return (*s)[n-1]
+}
